df099abaa6ed2c54941e9dd7cadf5b96f7173032
CBshmear <connorbeer13@gmail.com>
2022-12-26 15:10:15 -0600

    added questions to prompt list

--- Develop/index.js
+++ Develop/index.js
@@ -4,43 +4,82 @@ const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 //const questions = [];
+const generateReadMe = ({title, description, instructions, usage, contribution, test}) =>
+`# ${title}
+
+## Description
+
+${description}
+
+## Table of Contents 
+
+
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contributions](#Contribution)
+- [Testing](#Testing)
+- [License](#License)
+
+## Installation
+
+${instructions}
+
+## Usage
+
+${usage}
+
+## Contribution
+
+${contribution}
+
+## Testing
+
+${test}
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+---
+`
+
 inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      name: 'title',
+      message: 'Please enter a title.',
     },
     {
       type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
+      name: 'decription',
+      message: 'Please enter a description.',
     },
     {
       type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
+      name: 'instructions',
+      message: 'Please enter installation instructions.',
     },
     {
       type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
+      name: 'usage',
+      message: 'Please enter usage information.',
     },
     {
       type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
+      name: 'contribution',
+      message: 'Please enter contribution guidelines.',
     },
     {
       type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
+      name: 'test',
+      message: 'Please enter testing instructions.',
     },
   ])
   // TODO: Create a function to write README file
   // function writeToFile(fileName, data) {}
   .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+    const readMePageContent = generateReadMe(answers);
 
     fs.writeFile('README.md', readMePageContent, (err) =>
       err ? console.log(err) : console.log('Successfully created README.md!')
